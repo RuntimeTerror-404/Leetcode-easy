a7df455e267a3f53222ef7606ee2550df1af4291
Mohit Parashar <mohit1672002@gmail.com>
2021-03-17 23:14:53 +0530

    commit-2

--- easy-27.cpp
+++ easy-27.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 int main(){
     vector<int> v = {3, 2, 5, 3, 7, 9, 1, 3, 5, 99};
-    int val = 7;
-    vector<int> :: iterator it;
-    it = v.begin();
-    for(int i=0; i<v.size(); i++){
-        if(v[i] == val){
-            v.erase(it+i);
+    int val = 3;
+    // vector<int> :: iterator it;
+    // it = v.begin();
+    // for(int i=0; i<v.size(); i++){
+    //     if(v[i] == val){
+    //         v.erase(it+i);
+    //     }
+    // }
+    for(auto it=v.begin(); it!=v.end(); it++){
+        if(*it == val){
+            v.erase(it);
         }
     }
     cout<<v.size();
